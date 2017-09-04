6afe2856f21f4fad731130708bdf8876e557a3a9
Balaji J <baljanak@gmail.com>
2017-09-04 09:14:07 +0530

    set default font to inconsolata

--- init.el
+++ init.el
@@ -2,7 +2,6 @@
 (setq mac-option-modifier 'meta)
 (setq mac-command-modifier 'control)
 
-(require 'wgrep)
 (require 'ido)
 (setq ido-everywhere t)
 (setq ido-default-buffer-method 'selected-window)
@@ -25,3 +24,17 @@
 
 (load "$HOME/.emacs.d/go.el")
 (load "$HOME/.emacs.d/git.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (go-mode yaml-mode wgrep magit go-rename go-eldoc exec-path-from-shell dockerfile-mode company-go))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Inconsolata" :foundry "nil" :slant normal :weight normal :height 141 :width normal)))))
